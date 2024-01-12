985c53a6392349a4dcb0b288987a5df635b040a6
kennykenny19970314@gmail.com <kennykenny19970314@gmail.com>
2024-01-12 09:37:08 +0800

    build: route & controller setup

--- app.js
+++ app.js
@@ -3,7 +3,15 @@ const app = express()
 const port = 3000
 
 app.get('/',(req,res)=>{
+  res.redirect('/restaurants')
+})
+app.get('/restaurants',(req,res)=>{
   res.send('build successful')
+})
+
+app.get('/restaurant/:id',(req,res)=>{
+  const id = req.params.id 
+  res.send(`restaurant number : ${id}`)
 })
 
 app.listen(port,()=>{
