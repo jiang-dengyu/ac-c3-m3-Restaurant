623834edaddf2b70878b17b2dd420689d57c22b5
kennykenny19970314@gmail.com <kennykenny19970314@gmail.com>
2024-01-12 15:15:08 +0800

    feat: add listing page

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 const {engine} = require('express-handlebars')
+const restaurants = require('./public/jsons/restaurant.json').results
+
 
 app.engine('.hbs', engine({extname:'.hbs'}))
 app.set('view engine','.hbs')
@@ -12,7 +14,7 @@ app.get('/',(req,res)=>{
   res.redirect('/restaurants')
 })
 app.get('/restaurants',(req,res)=>{
-  res.render('index')
+  res.render('index',{restaurants})
 })
 
 app.get('/restaurant/:id',(req,res)=>{
@@ -21,5 +23,5 @@ app.get('/restaurant/:id',(req,res)=>{
 })
 
 app.listen(port,()=>{
-  console.log(`express server is runnning on http://localhost:${port}`)
+  console.log(`express server is running on http://localhost:${port}`)
 })
