761cbf1ae12d094d57276e3c0dcf011165f13e76
kennykenny19970314@gmail.com <kennykenny19970314@gmail.com>
2024-02-16 16:23:57 +0800

    modify todo migration file

--- migrations/20240216022805-create-store.js
+++ migrations/20240216022805-create-store.js
@@ -10,30 +10,39 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       name: {
+        allowNull: false,
         type: Sequelize.STRING
       },
       name_en: {
+        allowNull: false,
         type: Sequelize.STRING
       },
       category: {
+        allowNull: false,
         type: Sequelize.STRING
       },
       image: {
+        allowNull: false,
         type: Sequelize.STRING
       },
       location: {
+        allowNull: false,
         type: Sequelize.STRING
       },
       phone: {
+        allowNull: false,
         type: Sequelize.STRING
       },
       google_map: {
+        allowNull: false,
         type: Sequelize.STRING
       },
       rating: {
+        allowNull: false,
         type: Sequelize.STRING
       },
       description: {
+        allowNull: false,
         type: Sequelize.STRING
       },
       createdAt: {
