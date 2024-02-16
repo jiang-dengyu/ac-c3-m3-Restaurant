057d162e101d0b0816430959db0ba785382eb4e3
kennykenny19970314@gmail.com <kennykenny19970314@gmail.com>
2024-02-16 13:57:58 +0800

    連結mySQL資料庫與model

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
 const express = require('express')
 const app = express()
 const port = 3000
+
+const db = require('./models')
+const Stores = db.store
+
 const {engine} = require('express-handlebars')
-const restaurants = require('./public/jsons/restaurant.json').results
-
-
 app.engine('.hbs', engine({extname:'.hbs'}))
 app.set('view engine','.hbs')
 app.set('views','./views')
-app.use(express.static('public'))
 
+/*首頁全部清單*/
 app.get('/',(req,res)=>{
-  //res.redirect('/restaurants')
-  res.render('index',{restaurants})
+  return Stores.findAll()
+    .then((restaurant)=>res.send('index'))
+    .catch((err) => res.status(422).json(err))
 })
 
-app.get('/search',(req,res)=>{
+/*搜尋*/
+/*app.get('/search',(req,res)=>{
   const keyword = req.query.keyword || ' '
   const matchedRT = keyword ? restaurants.filter((RT) => 
     Object.values(RT).some((property) => {
@@ -26,14 +29,15 @@ app.get('/search',(req,res)=>{
     })  
   ):restaurants
   res.render('index', { restaurants: matchedRT, keyword })
-})
+})*/
 
-app.get('/restaurant/:id',(req,res)=>{
+/*單一detail*/
+/*app.get('/restaurant/:id',(req,res)=>{
   const id = req.params.id
   const restaurant = restaurants.find((restaurant) =>restaurant.id.toString() === id)
   res.render('detail',{restaurant})
 
-})
+})*/
 
 app.listen(port,()=>{
   console.log(`express server is running on http://localhost:${port}`)
