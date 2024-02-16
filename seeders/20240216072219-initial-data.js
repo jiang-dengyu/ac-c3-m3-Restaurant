e7da1b03b978b799dd575facfd7864f3d25fc7f2
kennykenny19970314@gmail.com <kennykenny19970314@gmail.com>
2024-02-16 21:15:28 +0800

    add todo seeder

--- seeders/20240216072219-initial-data.js
+++ seeders/20240216072219-initial-data.js
@@ -1,25 +1,29 @@
 'use strict';
+const json = require('../public/jsons/restaurant.json');
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    const newArray  = json.results.map((item)=>
+    {
+      return {
+        name: `${item.name}`,
+        name_en: `${item.name_en}`,
+        category: `${item.category}`,
+        image: `${item.image}`,
+        location: `${item.location}`,
+        phone: `${item.phone}`,
+        google_map: `${item.google_map}`,
+        rating: `${item.rating}`,
+        description: `${item.description}`,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }
+    })
+    await queryInterface.bulkInsert('stores',newArray)
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    await queryInterface.bulkDelete('stores', null)
   }
 };
