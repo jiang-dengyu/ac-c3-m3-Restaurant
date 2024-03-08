9c22d03b989708fde24b66aaabb2c1c6c1ff2c64
kennykenny19970314@gmail.com <kennykenny19970314@gmail.com>
2024-03-08 20:37:20 +0800

    implement login using passport

--- app.js
+++ app.js
@@ -1,33 +1,39 @@
-const express = require("express");
-const app = express();
-const port = 3000;
-const { engine } = require("express-handlebars");
-const methodOverride = require("method-override");
-const router = require("./routes");
-const flash = require("connect-flash");
-const session = require("express-session");
-const messageHandler = require("./middelwares/message-handler");
-const errorHandler = require("./middelwares/error-handler");
+const express = require('express')
+const app = express()
+const port = 3000
 
-app.engine(".hbs", engine({ extname: ".hbs" }));
-app.set("view engine", ".hbs");
-app.set("views", "./views");
+const { engine } = require('express-handlebars')
+const methodOverride = require('method-override')
 
-app.use(express.static("public"));
-app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride("_method"));
+const router = require('./routes')
+const passport = require('passport')
+
+const flash = require('connect-flash')
+const session = require('express-session')
+
+const messageHandler = require('./middelwares/message-handler')
+const errorHandler = require('./middelwares/error-handler')
+
+app.engine('.hbs', engine({ extname: '.hbs' }))
+app.set('view engine', '.hbs')
+app.set('views', './views')
+
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 app.use(
   session({
-    secret: "ThisIsStoresList",
+    secret: 'ThisIsStoresList',
     resave: false,
-    saveUninitialized: false,
+    saveUninitialized: false
   })
-);
-app.use(flash());
-app.use(messageHandler);
-app.use(router);
-app.use(errorHandler);
+)
+app.use(flash())
+app.use(passport.initialize())
+app.use(messageHandler)
+app.use(router)
+app.use(errorHandler)
 
 app.listen(port, () => {
-  console.log(`express server is running on http://localhost:${port}`);
-});
+  console.log(`express server is running on http://localhost:${port}`)
+})
--- routes/Users.js
+++ routes/Users.js
@@ -4,17 +4,51 @@ const router = express.Router()
 const db = require('../models')
 const User = db.User
 
-/* 登入頁面 *******************************************************/
+const passport = require('passport')
+const LocalStrategy = require('passport-local')
+/* LocalStrategy的策略流程***************************************** */
+passport.use(
+  new LocalStrategy({ usernameField: 'email' }, (username, passowrd, done) => {
+    return User.findOne({
+      attributes: ['id', 'name', 'email', 'password'],
+      where: { email: username },
+      raw: true
+    })
+      .then((user) => {
+        if (!user || user.passowrd != password) {
+          return done(null, false, { message: 'email或密碼錯誤' })
+        }
+        return done(null, user)
+      })
+      .catch((error) => {
+        error.message = '登入失敗'
+        done(null, error)
+      })
+  })
+)
+/* 成功後將登入資訊存入session***************************************** */
+passport.serializeUser((user, done) => {
+  const { id, name, email } = user
+  return done(null, { id, name, email })
+})
+/* 登入頁面 ****************************************************** */
 router.get('/login', (req, res) => {
   return res.render('login')
 })
-/* 登入後檢驗 *******************************************************/
-
-/* 註冊頁面 *******************************************************/
+/* 登入後檢驗 ****************************************************** */
+router.post(
+  '/login',
+  passport.authenticate('local', {
+    successRedirect: '/Stores',
+    failureRedirect: '/Users/login',
+    failureMessage: true
+  })
+)
+/* 註冊頁面 ****************************************************** */
 router.get('/register', (req, res) => {
   return res.render('register')
 })
-/* 註冊內容檢驗 ****************************************************/
+/* 註冊內容檢驗 *************************************************** */
 router.post('/register', (req, res, next) => {
   const { email, name, password, confirmPassword } = req.body
 
